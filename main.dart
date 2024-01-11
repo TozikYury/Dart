f2e6ed8c7e42057384fdf6cfbd45b77af3c863d5
TozikYury <tozik.2024@mail.ru>
2024-01-11 22:53:29 +0300

    Сортировка объектов (рабоющие)

--- main.dart
+++ main.dart
@@ -2,7 +2,18 @@ import 'Employee.dart';
 import 'Person.dart';
 
 void main(List<String> args) {
-  final people = <Person>[Person(name: 'Kate',age: 22),
-  Employee(age: 26, name: 'Dasha', works: 'Undex'),
-  Person(age: 25, name: 'Dima')];
+  final people = <Person>[
+    Person(name: 'Kate', age: 22),
+    Employee(age: 26, name: 'Dasha', works: 'Undex'),
+    Person(age: 25, name: 'Dima'),
+    Person(age: 34, name: 'Viktor'),
+    Employee(age: 45, name: 'Masha', works: 'Google'),
+  ];
+
+  print('Работающая группа');
+  people.forEach((element) {
+    if (element is Employee) {
+      print(element);
+    }
+  });
 }
