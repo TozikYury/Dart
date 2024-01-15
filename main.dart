73ab396eb1d2f88459772a7bca82a8c2c26bc97b
TozikYury <tozik.2024@mail.ru>
2024-01-15 17:01:13 +0300

    Обработка исключений

--- main.dart
+++ main.dart
@@ -1,11 +1,18 @@
-import 'Person.dart';
-
 void main(List<String> args) {
-  final person = <Person>[
-    Person(age: 22, name: 'Tozik'),
-    Person(age: 34, name: 'Kate')
-  ];
-  person.forEach((element) {
-    print(element);
-  });
+  var numberOne = 12;
+  var numberTwo = 'sdhk';
+  try {
+    var count = numberOne / numberTwo.toDouble();
+    print(count.toStringAsFixed(3));
+  } on Exception catch (e) {
+    print('Error: $e');
+  } finally {
+    print('Close');
+  }
+}
+
+extension on String {
+  double toDouble() {
+    return double.parse(this);
+  }
 }
