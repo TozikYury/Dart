d3639569ef7c4f8a265ecfccce10b361704f68ca
TozikYury <tozik.2024@mail.ru>
2024-01-15 22:24:24 +0300

    Создание своего исключения

--- main.dart
+++ main.dart
@@ -1,18 +1,34 @@
-void main(List<String> args) {
-  var numberOne = 12;
-  var numberTwo = 'sdhk';
-  try {
-    var count = numberOne / numberTwo.toDouble();
-    print(count.toStringAsFixed(3));
-  } on Exception catch (e) {
-    print('Error: $e');
-  } finally {
-    print('Close');
+class IncorrectValues {
+  @override
+  String toString() {
+    return 'Не верные данные!';
   }
 }
 
-extension on String {
-  double toDouble() {
-    return double.parse(this);
+class DivbyZero {
+  @override
+  String toString() {
+    return 'Делить на 0 нельзя!';
   }
+}
+
+void main(List<String> args) {
+  try {
+    var count = div('238727', '0');
+    print(count.toStringAsFixed(3));
+  } on IncorrectValues catch (e) {
+    print(e);
+  } on DivbyZero catch (e) {
+    print(e);
+  } catch (e) {
+    print(e);
+  }
+}
+
+double div(String numberOne, String numberTwo) {
+  final numOne = int.tryParse(numberOne);
+  final numTwo = int.tryParse(numberTwo);
+  if (numOne == null || numTwo == null) throw IncorrectValues();
+  if (numTwo == 0) throw DivbyZero();
+  return numOne / numTwo;
 }
