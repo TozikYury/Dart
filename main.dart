a9133c389aa83c0ed91552adda2bbd4555a2a049
TozikYury <tozik.2024@mail.ru>
2024-01-15 22:35:22 +0300

    Поиск неверного значения

--- main.dart
+++ main.dart
@@ -1,7 +1,11 @@
 class IncorrectValues {
+  final String number;
+
+  IncorrectValues(this.number);
+
   @override
   String toString() {
-    return 'Не верные данные!';
+    return 'Не верные данные! $number';
   }
 }
 
@@ -14,7 +18,7 @@ class DivbyZero {
 
 void main(List<String> args) {
   try {
-    var count = div('238727', '0');
+    var count = div('23hjbu', '8');
     print(count.toStringAsFixed(3));
   } on IncorrectValues catch (e) {
     print(e);
@@ -28,7 +32,8 @@ void main(List<String> args) {
 double div(String numberOne, String numberTwo) {
   final numOne = int.tryParse(numberOne);
   final numTwo = int.tryParse(numberTwo);
-  if (numOne == null || numTwo == null) throw IncorrectValues();
+  if (numOne == null) throw IncorrectValues(numberOne);
+  if (numTwo == null) throw IncorrectValues(numberTwo);
   if (numTwo == 0) throw DivbyZero();
   return numOne / numTwo;
 }
